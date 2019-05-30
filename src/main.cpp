548a8e4cbba1f7a2b652d3d6ca8de4a9be4f3760
Qusai <kussiadebfx@gmail.com>
2019-05-30 09:51:07 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -50,7 +50,7 @@ void init () {
         if (startX < 0)
             startX = 0;
     }
-    game->newGame(startX, startX, height, width, isVerticale);
+    game->newGame(startaY, startX, height, width, isVerticale);
     game->playGame();
 }
 
