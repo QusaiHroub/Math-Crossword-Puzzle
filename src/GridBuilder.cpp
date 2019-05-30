83f641bb63c4ab9200fc08f5f47eb03f063da53d
Qusai <kussiadebfx@gmail.com>
2019-05-30 07:02:41 +0300

    Update GridBuilder.cpp

--- src/GridBuilder.cpp
+++ src/GridBuilder.cpp
@@ -1,3 +1,21 @@
+/* mathCrossWordPuzzle
+* Copyright 2019 Qusai Hroub.
+*
+* This file is part of the mathCrossWordPuzzle.
+*
+* Authors:
+* Qusai Hroub <qusaihroub.r@gmail.com>
+*
+* This program is free software; you can redistribute it and/or modify it
+* under the terms and conditions of the GNU General Public License,
+* version 3, as published by the Free Software Foundation.
+*
+* This program is distributed in the hope it will be useful, but WITHOUT ANY
+* WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+* FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+* more details.
+*/
+
 #include "GridBuilder.hpp"
 #include <vector>
 #include <queue>
