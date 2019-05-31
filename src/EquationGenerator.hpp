f86a812d5523169526f24997a3e30e183c2726a2
Qusai <kussiadebfx@gmail.com>
2019-05-31 10:34:08 +0300

    Update EquationGenerator.hpp

--- src/EquationGenerator.hpp
+++ src/EquationGenerator.hpp
@@ -28,13 +28,13 @@ private:
     int m_width, m_height, m_startY, m_startX, m_maxNumberLength = 7;
     vector < vector < CoreCell * > > *m_adjMatrix;
     vector < vector < bool > > m_visited;
-    const string m_oList[4] = {"+", "×", "-", "÷"};
+    const string m_oList[4] = {"+", "Ã—", "-", "Ã·"};
     int x_d[4] = {0,1,-1,0};
     int y_d[4] = {1,0,0,-1};
     int nextCell[3] = {0, 2 , 4};
     void DFS (const int x, const int y, bool isV);
-    string detOpr (int **eNumber, int numberOfNonZero);
-    void detNum (int **eNumber, int n, string o);
+    string detOpr (int **eNumber, int numberOfNONnull);
+    void detNum (int **eNumber, int numberOfNONnull, string o);
     void setMathEQ (int y, int x, int **eNumber, bool isV);
     int gcd(int a, int b);
     void setMaxNumberLength(int newLength);
